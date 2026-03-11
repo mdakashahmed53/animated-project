d5d338710aa6ac57f116a1ada017006aa376b9cc
Md. Akash Ahmed <mdakash30453@gmail.com>
2026-03-11 15:51:26 +0600

    project

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:animated_app/home.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animated app',
+      home: Home(),
+    );
+  }
+}
