0b648ecbb28b0776b52e8abeb70f41e0a3e23005
Md. Akash Ahmed <mdakash30453@gmail.com>
2026-03-11 22:16:26 +0600

    animate word

--- lib/home.dart
+++ lib/home.dart
@@ -8,7 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-      bool selected = false;
+  bool selected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,21 +22,36 @@ class _HomeState extends State<Home> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () {
+            AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              height: selected ? 150 : 150,
+              width: selected ? 150 : 150,
+
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(selected ? 10: 30),
+                color: selected ? Colors.amberAccent : Colors.green
+              ),
+              // child: Container(
+              //   height: 150,
+              //   // width: 150,
+              //   // decoration: BoxDecoration(
+              //   //   color: Colors.amber,
+              //   //   borderRadius: BorderRadius.circular(10)
+              //   // ),
+              // ),
+            ),
+
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
                 setState(() {
                   selected = !selected;
                 });
               },
-              child: AnimatedContainer(
-                duration: Duration(seconds: 1),
-                height: selected ? 150 : 300,
-                width: selected ? 150 : 300,
-                
-                decoration: BoxDecoration(),
-                child: Container(height: 150, width: 150, color: selected ? Colors.amberAccent : Colors.red),
-              ),
+              child: Text('Animate'),
             ),
+
+            A
           ],
         ),
       ),
